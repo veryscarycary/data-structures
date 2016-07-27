e54d73455c01f14cde262e0c7b6db7281cd52901
Simeon Lee <simeonhlee@gmail.com>
2016-07-27 11:50:56 -0700

    Add size variable to functional/stack.js with functionality
    Add size variable that increments and decrements (maximum of 0)
    when push and pop methods are called and returns the size
    variable when size method is called

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,21 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size++;
   };
 
   someInstance.pop = function() {
+    if ( size - 1 >= 0) {
+      size--;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
