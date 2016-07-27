43b482b5257209d7608e5dd3411f66c97d7f488c
Simeon Lee <simeonhlee@gmail.com>
2016-07-27 11:58:24 -0700

    Add functionality that allows push and pop of functional/stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -7,12 +7,19 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    if ( value ) {
+      storage[size] = value;
+    }
     size++;
   };
 
   someInstance.pop = function() {
+    var temp;
     if ( size - 1 >= 0) {
       size--;
+      temp = storage[size];
+      storage[size] = undefined;
+      return temp;
     }
   };
 
