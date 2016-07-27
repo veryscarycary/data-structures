51ead308a7346efebc0bca07bf9411ec781e4a79
veryscarycary <carymeskell@gmail.com>
2016-07-27 14:53:06 -0700

    Implement functionality so that .size returns 0

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var theInstance = {};
+
+  var storage = {};
+
+
+  theInstance.push = function(val) {
+
+  };
+
+  theInstance.pop = function() {
+
+  };
+
+  theInstance.size = function() {
+    return 0;
+  };
+
+
+
+
+  return theInstance;
 };
 
 var stackMethods = {};
