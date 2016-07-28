ebf56b0319965263670e250dd033e449966f1bea
veryscarycary <carymeskell@gmail.com>
2016-07-28 11:40:54 -0700

    Complete part of trees

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -12,10 +14,24 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   // your code here
-  newTree.children = null;  // fix me
+  this.children.push(Tree(value));  // fix me
 };
 
 treeMethods.contains = function(target) {
+  var binarySearch = function(array, target, startIndex, endIndex) {
+    var startIndex = startIndex || 0;
+    var endIndex = endIndex || array.length - 1;
+    var midpoint = Math.floor((endIndex+ startIndex)/2);
+    if (array[midpoint] === target) {
+      return midpoint;
+    }
+    if (array[midpoint] > target) {
+      // recurse left
+    } else {
+      // recurse right
+    }
+  };
+
 };
 
 
