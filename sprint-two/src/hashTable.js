514241feff7aa98c23bccac86f7d626f9c9d8743
veryscarycary <carymeskell@gmail.com>
2016-07-28 20:26:39 -0700

    Implement hash table functionality

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,44 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if ( this._storage[index] === undefined ) {
+    this._storage[index] = [];
+  }
+  for (var i = 0; i < this._storage[index].length; i++ ) {
+    var tuple = this._storage[index][i];
+    var key = tuple[0];
+    if ( key === k ) {
+      tuple[1] = v;
+      return;
+    }
+  }
+  this._storage[index].push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
+  console.log(bucket);
+  for ( var i = 0; i < bucket.length; i++ ) {
+    var tuple = bucket[i];
+    var key = tuple[0];
+    var val = tuple[1];
+    if ( key === k ) {
+      return val;
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
+  for ( var i = 0; i < bucket.length; i++ ) {
+    var tuple = bucket[i];
+    var key = tuple[0];
+    if ( key === k ) {
+      bucket.splice(i, 1);
+    }
+  }
 };
 
 
